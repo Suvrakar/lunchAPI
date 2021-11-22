b7478af4e6020415541c33d69ac123d8edc453dd
Suvra <kar.suvra@gmail.com>
2021-11-22 17:08:53 +0600

    swager

--- Common/dbConection.js
+++ Common/dbConection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connect = async() => {
     try {
-        let res=await mongoose.connect("mongodb://localhost:27017/HivecoreLunch", { useNewUrlParser: true, useUnifiedTopology: true, });
+        let res=await mongoose.connect("mongodb+srv://HivecoreLunch:hivecorelimited@cluster0.tz1pa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, });
         console.log("Connected");
     } catch (error) {
         console.log("Not Connected ,", error);
